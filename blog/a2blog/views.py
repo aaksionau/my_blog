c84c8f5a8a97dc736040613cee1229aa1b46c77e
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2019-09-06 10:36:14 -0500

    class based views

--- blog/a2blog/views.py
+++ blog/a2blog/views.py
@@ -1,27 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
 
-def post_list(request):
-
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 5)
-    page = request.GET.get('page')
-
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # if the page is not an integer deliver first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page is out of range deliver last page
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request,
-                  'a2blog/post/list.html',
-                  {'page': page, 'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'a2blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
