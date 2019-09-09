bf741cbb8f8a925cd28d47edc69757cd59ea48b7
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2019-09-09 10:47:02 -0500

    Adds feed to blog

--- blog/a2blog/urls.py
+++ blog/a2blog/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
+
 
 app_name = 'a2blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('tag/<slug:tag>/', views.PostFilteredListView.as_view(), name='post_list_by_tag'),
+    path('tag/<slug:tag>/', views.PostFilteredListView.as_view(),
+         name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
