99082990aa70dd955db9fb5a60a0fea5e128b28d
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2019-09-09 10:34:51 -0500

    Adds sitemap

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'a2blog.apps.A2BlogConfig',
     'taggit',
 ]
@@ -125,3 +127,5 @@ STATICFILES_DIRS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
